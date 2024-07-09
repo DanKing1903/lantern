b48866725fa8461676dbcff824c7a2fd15ec7828
Daniel King <32928632+DanKing1903@users.noreply.github.com>
2024-07-09 13:08:44 +0100

    feat: create reports API endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,13 @@
-class TestAPI:
-    def test_foo(self):
-        assert "FOO" == "foo".upper()
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.post(
+        "/reports/",
+        json={"file_path": "assets/financellc.pdf", "company_name": "FinanceLLC"},
+    )
+    assert response.status_code == 200
